70fd92c00c075c6c77e9d019bfd50816399f7653
Lolu Adelore <adeloreisaac@gmail.com>
2021-10-17 17:21:41 +0100

    initial commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(2),
+            'description' => $this->faker->sentence(15),
+            'price' => $this->faker->numberBetween(10,50),
+        ];
+    }
+}
