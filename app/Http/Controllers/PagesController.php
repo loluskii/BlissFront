ada6b294e81d23015cbcdfbe91b8d064db95158f
Lolu Adelore <adeloreisaac@gmail.com>
2021-12-11 20:54:20 +0100

    Revert "Eloquent filter"

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,7 +6,6 @@ use App\Models\Store;
 use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
-use App\Services\Filters\FilterQueries;
 
 class PagesController extends Controller
 {
@@ -29,18 +28,5 @@ class PagesController extends Controller
     public function subscribe(){
         $cartItems = \Cart::session(auth()->id())->getContent();
         return view('store.subscribe', compact('cartItems'));
-    }
-
-    public function filter(){
-        try {
-            $products = (new FilterQueries())->filter();
-            dd($products);
-            // return view('search.index')->with('events', $events)->with('interests', $interests);
-        } catch (\Exception $e) {
-            return back()->with(
-                'error',
-                $e->getMessage()
-            );
-        }
     }
 }
