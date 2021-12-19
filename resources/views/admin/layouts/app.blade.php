7800fd6efaa495ca171cb563d656e52f66972c15
Lolu Adelore <adeloreisaac@gmail.com>
2021-12-19 21:45:54 +0100

    updates

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Enterprise Dashboard</title>
+    <title>Bliss Dashboard</title>
     <link rel="stylesheet" href="{{ secure_asset('plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('dist/css/sb-admin-2.min.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
